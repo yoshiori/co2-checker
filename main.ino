e3ec62bfb00bffcdc2076e29ea3890ab76beaf94
Yoshiori SHOJI <yoshiori@gmail.com>
2022-10-23 01:36:04 +0900

    Use Doublebuffer

--- main.ino
+++ main.ino
@@ -6,6 +6,9 @@
 long CONSOL_BAND = 115200;
 Adafruit_SGP30 sgp;
 
+// Doublebuffer
+TFT_eSprite canvas = TFT_eSprite(&M5.Lcd);
+
 void setup()
 {
   M5.begin();
@@ -24,17 +27,15 @@ void setup()
   M5.Lcd.setTextColor(WHITE, BLACK);
   M5.Lcd.setTextDatum(TL_DATUM);
   M5.Lcd.setCursor(20, 40);
-  for(int i = 0; i < 15; i++){
-    M5.Lcd.printf(".");
-    delay(1000);
-  }
 
   CONSOLE.println("Setup finish");
+  canvas.setColorDepth(8);
+  canvas.createSprite(M5.Lcd.width(), M5.Lcd.height());
 }
 
 void loop()
 {
-  M5.Lcd.fillScreen(BLACK);
+  canvas.fillScreen(BLACK);
   if (!sgp.IAQmeasure())
   {
     CONSOLE.println("Measurement failed");
@@ -49,6 +50,7 @@ void loop()
   drawPower(0, 0);
   drawTVOC(0, 60);
   drawECO2(160, 60);
+  canvas.pushSprite(0, 0);
   delay(1000);
 }
 
@@ -59,12 +61,12 @@ static const int32_t FONT_6_H = 48;
 static const int32_t FONT_6_W = 32;
 
 void drawECO2(int32_t x, int32_t y){
-  M5.Lcd.setTextColor(GREENYELLOW, BLACK);
-  M5.Lcd.fillRect(x, y, 4, FONT_4_H * 2 + PADDING_H + FONT_6_H, GREENYELLOW);
-  M5.Lcd.drawString("eCO2", x + 6, y + PADDING_H, 4);
+  canvas.setTextColor(GREENYELLOW, BLACK);
+  canvas.fillRect(x, y, 4, FONT_4_H * 2 + PADDING_H + FONT_6_H, GREENYELLOW);
+  canvas.drawString("eCO2", x + 6, y + PADDING_H, 4);
   int16_t eco2 = sgp.eCO2;
   int32_t eco2_x = x + 6;
-  M5.Lcd.setTextColor(WHITE, BLACK);
+  canvas.setTextColor(WHITE, BLACK);
   if(eco2 < 1000){
     eco2_x += FONT_6_W;
   }
@@ -77,20 +79,20 @@ void drawECO2(int32_t x, int32_t y){
 
   // Warning value
   if(eco2 > 1000){
-    M5.Lcd.setTextColor(RED, BLACK);
+    canvas.setTextColor(RED, BLACK);
   }
-  M5.Lcd.drawString(String(eco2), eco2_x, y + PADDING_H + FONT_4_H, 7);
-  M5.Lcd.setTextColor(GREENYELLOW, BLACK);
-  M5.Lcd.drawString("ppm", x + 6 + FONT_6_W * 3 + 5, y + PADDING_H + (FONT_4_H * 3), 4);
+  canvas.drawString(String(eco2), eco2_x, y + PADDING_H + FONT_4_H, 7);
+  canvas.setTextColor(GREENYELLOW, BLACK);
+  canvas.drawString("ppm", x + 6 + FONT_6_W * 3 + 5, y + PADDING_H + (FONT_4_H * 3), 4);
 }
 
 void drawTVOC(int32_t x, int32_t y){
-  M5.Lcd.setTextColor(GREENYELLOW, BLACK);
-  M5.Lcd.fillRect(x, y, 4, FONT_4_H * 2 + PADDING_H + FONT_6_H, GREENYELLOW);
-  M5.Lcd.drawString("TVIC", x + 6, y + PADDING_H, 4);
+  canvas.setTextColor(GREENYELLOW, BLACK);
+  canvas.fillRect(x, y, 4, FONT_4_H * 2 + PADDING_H + FONT_6_H, GREENYELLOW);
+  canvas.drawString("TVIC", x + 6, y + PADDING_H, 4);
   int16_t tvoc = sgp.TVOC;
   int32_t tvoc_x = x + 6 + FONT_6_W;
-  M5.Lcd.setTextColor(WHITE, BLACK);
+  canvas.setTextColor(WHITE, BLACK);
   if(tvoc < 100){
     tvoc_x += FONT_6_W;
   }
@@ -100,45 +102,45 @@ void drawTVOC(int32_t x, int32_t y){
 
   // Warning value
   if(tvoc > 89){
-    M5.Lcd.setTextColor(RED, BLACK);
+    canvas.setTextColor(RED, BLACK);
   }
 
-  M5.Lcd.drawString(String(tvoc), tvoc_x, y + PADDING_H + FONT_4_H, 7);
-  M5.Lcd.setTextColor(GREENYELLOW, BLACK);
-  M5.Lcd.drawString("ppb", x + 6 + FONT_6_W * 3 + 5, y + PADDING_H + (FONT_4_H * 3), 4);
+  canvas.drawString(String(tvoc), tvoc_x, y + PADDING_H + FONT_4_H, 7);
+  canvas.setTextColor(GREENYELLOW, BLACK);
+  canvas.drawString("ppb", x + 6 + FONT_6_W * 3 + 5, y + PADDING_H + (FONT_4_H * 3), 4);
 }
 
 void drawPower(int32_t x, int32_t y)
 {
-  M5.Lcd.setTextColor(GREENYELLOW, BLACK);
-  M5.Lcd.drawString("POWER", x + 1, y + 3, 4);
+  canvas.setTextColor(GREENYELLOW, BLACK);
+  canvas.drawString("POWER", x + 1, y + 3, 4);
   int32_t second_line_y = y + 3 + FONT_4_H + PADDING_H;
   if (M5.Power.isCharging())
   {
-    M5.Lcd.setTextColor(YELLOW, BLACK);
-    M5.Lcd.drawString("EXTERNAL", x + 1, second_line_y, 4);
+    canvas.setTextColor(YELLOW, BLACK);
+    canvas.drawString("EXTERNAL", x + 1, second_line_y, 4);
   }
   else
   {
-    M5.Lcd.setTextColor(RED, BLACK);
-    M5.Lcd.drawString("INTERNAL", x + 1, second_line_y, 4);
+    canvas.setTextColor(RED, BLACK);
+    canvas.drawString("INTERNAL", x + 1, second_line_y, 4);
   }
-  M5.Lcd.drawFastVLine(x + 140 + 2, y + 1, (FONT_4_H + PADDING_H) * 2, GREENYELLOW);
-  M5.Lcd.setTextColor(WHITE, BLACK);
+  canvas.drawFastVLine(x + 140 + 2, y + 1, (FONT_4_H + PADDING_H) * 2, GREENYELLOW);
+  canvas.setTextColor(WHITE, BLACK);
   int8_t battery = M5.Power.getBatteryLevel();
   int32_t battery_x = x + 180;
   if (battery < 100)
   {
-    M5.Lcd.setTextColor(YELLOW, BLACK);
+    canvas.setTextColor(YELLOW, BLACK);
     battery_x += FONT_6_W;
   }
   if (battery < 10)
   {
-    M5.Lcd.setTextColor(RED, BLACK);
+    canvas.setTextColor(RED, BLACK);
     battery_x += FONT_6_W;
   }
-  M5.Lcd.drawString(String(battery), battery_x, y + 3, 7);
-  M5.Lcd.setTextColor(GREENYELLOW, BLACK);
-  M5.Lcd.drawString("%", x + 280, second_line_y, 4);
-  M5.Lcd.drawRect(x, y, 320, (FONT_4_H + PADDING_H) * 2, GREENYELLOW);
+  canvas.drawString(String(battery), battery_x, y + 3, 7);
+  canvas.setTextColor(GREENYELLOW, BLACK);
+  canvas.drawString("%", x + 280, second_line_y, 4);
+  canvas.drawRect(x, y, 320, (FONT_4_H + PADDING_H) * 2, GREENYELLOW);
 }
